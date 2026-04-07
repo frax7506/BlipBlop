0870a42cf00b7622f75d66d5b34ebf1b2fa89433
Hjalmar Danielsson <hjalmar.danielsson@learnet.se>
2026-04-07 16:43:38 +0200

    Adding some first time setup files

---
+++ Premake/premake5.lua
@@ -0,0 +1,29 @@
+workspace("BlipBlop")
+	configurations({"Debug", "Release"})
+	platforms({"x64"})
+	location("..")
+	
+project("BlipBlop")
+	kind("StaticLib")
+	language("C++")
+	location("../BlipBlop")
+	files("../BlipBlop/*.h")
+	files("../BlipBlop/*.cpp")
+	pchheader("stdafx.h")
+	pchsource("../BlipBlop/stdafx.cpp")
+	objdir("../Intermediate/BlipBlop")
+	
+	filter({"configurations:Debug"})
+		targetname("BlipBlop_Debug")
+		targetdir("../Output/BlipBlop")
+	
+	filter({"configurations:Release"})
+		targetname("BlipBlop_Release")
+		targetdir("../Output/BlipBlop")
+		
+	filter({"configurations:*"})
+		
+	warnings("Extra")
+	fatalwarnings({"All"})
+	
+	defines { "_CRT_SECURE_NO_WARNINGS" }
